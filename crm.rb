9be0b730ceabd103d09ff7922b42c081150071c7
Moufid Larbi <moufid4@gmail.com>
2014-08-23 12:06:28 -0400

    Created new classes and methods

---
+++ crm.rb
@@ -0,0 +1,57 @@
+class CRM
+
+	def initialize
+		
+	end
+
+	def print_menu
+		
+	end
+
+	def main_menu
+		
+	end
+
+	def call_options
+		
+	end
+end
+
+class Rolodex
+	
+	def initialize
+		
+	end
+
+	def add_contact
+
+	end
+
+	def display_all
+		
+	end
+
+	def display_one
+	
+	end
+
+	def display_attribute
+		
+	end
+end
+
+class Contact
+
+	def initialize
+		
+	end
+
+	def new_contact
+		
+	end
+
+	def modify_contact
+		
+	end
+
+end
